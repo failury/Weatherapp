61c68d0f3fef6386fe053d275ff81c3c6f9ee50c
failury <failury29@gmail.com>
2020-01-02 19:36:59 -0800

    Initial Commit

---
+++ Weatherapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weatherapp
+//
+//  Created by failury on 1/2/20.
+//  Copyright © 2020 failury. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
